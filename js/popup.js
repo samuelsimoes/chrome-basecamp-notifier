430a625ca913e0ed3ac326541921f3294988bd56
Samuel Simões <slip.cm@gmail.com>
2013-07-30 13:57:50 -0300

    Added the link to configs page in popup

--- js/popup.js
+++ js/popup.js
@@ -8,6 +8,13 @@ require(["common"], function (common) {
     AuthPopupView,
     UserToken
   ){
+
+    $("#configs_button")
+      .on("click", function() {
+        chrome.tabs.create({ url: chrome.extension.getURL('options.html') });
+        return false;
+      });
+
     if (UserToken.current() == undefined) {
       return new AuthPopupView().render();
     } else {
