b1d82c6f60531de2d77f6bda8ad2628840c5fad8
Samuel Simões <slip.cm@gmail.com>
2015-05-23 23:21:43 -0300

    Blank slate to the events list on the popup

--- src/js/components/popup/events.jsx
+++ src/js/components/popup/events.jsx
@@ -9,12 +9,25 @@ define("components/popup/events", ["react", "jsx!components/popup/event"], funct
                     bucketName={data.bucket.name} />;
     },
 
-    render: function() {
+    renderEvents: function() {
       return (
         <ul className="notification-list">
           {this.props.events.map(this.renderEvent)}
         </ul>
       );
+    },
+
+    renderBlankSlate: function() {
+      return (
+        <div className="notification-list-blank-slate">
+          <i className="icon icon-remove-sign"></i>
+          Sorry, no events here.
+        </div>
+      );
+    },
+
+    render: function() {
+      return this.props.events.length ? this.renderEvents() : this.renderBlankSlate();
     }
   });
 });
