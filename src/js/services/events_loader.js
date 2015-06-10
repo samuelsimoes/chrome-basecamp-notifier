d20982e13d94b5740518fe90cddac11e22cc55d6
Samuel Simões <slip.cm@gmail.com>
2015-06-10 19:30:29 -0300

    Adjusment on events loader

--- src/js/services/events_loader.js
+++ src/js/services/events_loader.js
@@ -1,12 +1,64 @@
 define([
   "services/events_endpoint",
-  "services/authenticated_ajax"
+  "services/authenticated_ajax",
+  "services/defer"
 ], function(
   EventsEndpoint,
-  AuthenticatedAjax
+  AuthenticatedAjax,
+  Defer
 ) {
+  /**
+   * Sadly it's one of most complicated routines of the app.
+   *
+   * First of all, Basecamp only returns 50 items per page, so, remember that.
+   *
+   * Imagine that you stay way from you computer for a couple hours and while it
+   * away time your team generate more than 50 events. So you need make a request
+   * to events endpoint since the last event passed on the arguments and verifies if it
+   * has a subsequent page to load the remaining events and it must be recursive,
+   * once the away time can generated an unknow amount of pages.
+   *
+   * Important notice that if the "since" isn't present (most common on the first
+   * app request) the routine finish on the first payload.
+   */
   return function(accountID, options) {
-    var url = EventsEndpoint(accountID);
-    return AuthenticatedAjax(url, options);
+    var defer = Defer(),
+        options = options || {},
+        since = options.since,
+        url = EventsEndpoint(accountID);
+
+    var OnError = function(data, xhr) {
+      if (xhr === 404) {
+        defer.resolve(data);
+      } else {
+        defer.reject(xhr);
+      }
+    };
+
+    var OnLoad = function(aggregatedData, page, eventsData) {
+      aggregatedData = aggregatedData.concat(eventsData);
+
+      if (eventsData.length < 50 || !since) {
+        defer.resolve(aggregatedData);
+      } else {
+        var nextPage = (page + 1);
+
+        AuthenticatedAjax(url, {
+          queryString: { since: since, page: nextPage }
+        }).then(
+          OnLoad.bind(null, aggregatedData, nextPage),
+          OnError.bind(null, aggregatedData)
+        );
+      }
+    };
+
+    AuthenticatedAjax(url, {
+      queryString: { since: options.since }
+    }).then(
+      OnLoad.bind(null, [], 1),
+      OnError.bind(null, null)
+    );
+
+    return defer.promise;
   };
 });
