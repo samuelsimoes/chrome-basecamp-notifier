c5b77a23385eff3818ceaecbdb572b6de894dd30
Samuel Simões <slip.cm@gmail.com>
2015-06-12 23:49:10 -0300

    Added the notification about the non notified events per request

--- src/js/services/events_notifiers.js
+++ src/js/services/events_notifiers.js
@@ -2,11 +2,13 @@ define([
   "services/array_local_storage",
   "services/event_desktop_notification",
   "services/object_local_storage",
+  "services/desktop_notification",
   "services/badge"
 ], function(
   ArrayLocalStorage,
   EventDesktopNotification,
   ObjectLocalStorage,
+  DesktopNotification,
   Badge
 ) {
   var MAX_DESKTOP_NOTIFICATIONS = 5;
@@ -27,5 +29,15 @@ define([
     firstEvents.forEach(function(eventData) {
       EventDesktopNotification.notify(eventData);
     });
+
+    var nonNotifiedEventsCount = (eventsData.length - MAX_DESKTOP_NOTIFICATIONS);
+
+    if (eventsData.length > MAX_DESKTOP_NOTIFICATIONS) {
+      DesktopNotification({
+        title: "Basecamp Notifier",
+        message: ("And more " + nonNotifiedEventsCount + " event(s)."),
+        iconUrl: "img/icons/64.png"
+      });
+    }
   }
 });
